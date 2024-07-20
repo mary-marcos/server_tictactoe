606f019116c1e7256ac458850b232b0b435b2c68
moneer <moneerahmes14@gmail.com>
2024-07-20 17:47:55 +0300

    creat class Dal_1 and mohamed creat method isplayer exist and status and ahmed naser score and is player exist

---
+++ src/serverapp/DAL_1.java
@@ -0,0 +1,220 @@
+package serverapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.derby.jdbc.ClientDriver;
+
+
+
+
+public class DAL_1 {
+    
+    private static Connection con;
+    private static Vector<UserTable> userList = new Vector<>();
+    private static boolean userExist;
+    
+       public static void stablishConnection()
+       {
+           try {
+               DriverManager.registerDriver(new ClientDriver());
+               con = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe", "tic", "tic");
+           } catch (SQLException ex) {
+               Logger.getLogger(DAL_1.class.getName()).log(Level.SEVERE, null, ex);
+           }
+       }
+       
+    public static void getAllData() throws SQLException {
+        
+        stablishConnection();
+       String query = "SELECT * FROM USERTABELM";
+       
+       PreparedStatement pst = con.prepareStatement(query);
+       ResultSet rs = pst.executeQuery();
+       
+        while (rs.next()) {
+            String userName = rs.getString(1);
+            String password = rs.getString(2);
+            String email = rs.getString(3);
+            int score = rs.getInt(6);
+            boolean status = rs.getBoolean(5);
+            boolean availableity = rs.getBoolean(4);
+            UserTable user = new UserTable(userName, password, email, score, status,availableity);
+            userList.add(user);
+        }
+        
+            rs.close();
+            pst.close();
+            con.close();
+        
+    }
+
+    public static void update(UserTable user) throws SQLException {
+        
+        stablishConnection();
+        String sql = "UPDATE USERTABELM SET PASSWORD=?, EMAIL=?, SCORE=?, STATUS=? WHERE USERNAME=?";
+        PreparedStatement pst = con.prepareStatement(sql); 
+        
+        pst.setString(1, user.getPassword());
+        pst.setString(2, user.getEmail());
+        pst.setInt(3, user.getScore());
+        pst.setBoolean(4, user.isStatus());
+        pst.setString(5, user.getUserName());
+        pst.setBoolean(6, true);
+        
+        pst.executeUpdate();
+        
+        pst.close();
+        con.close();
+        
+    }
+
+    public static void insert(UserTable user) throws SQLException {
+        
+           stablishConnection();
+        String query = "INSERT INTO USERTABELM (USERNAME, PASSWORD, EMAIL, SCORE, STATUS,ISAVALIABLE) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement pst = con.prepareStatement(query);
+        
+        pst.setString(1, user.getUserName());
+        pst.setString(2, user.getPassword());
+        pst.setString(3, user.getEmail());
+        pst.setInt(4, 0);
+        pst.setBoolean(5, false);
+        pst.setBoolean(6, true);
+        
+        pst.executeUpdate();
+        
+        pst.close();
+        con.close();
+    }
+
+    public static UserTable search(String username) throws SQLException {
+     
+        stablishConnection();
+        String query = "SELECT USERNAME, PASSWORD, EMAIL, SCORE, STATUS FROM USERTABELM WHERE USERNAME = ?";
+        PreparedStatement pst = con.prepareStatement(query);
+        pst.setString(1, username);
+
+        ResultSet rs = pst.executeQuery();
+        UserTable user = null;
+        if (rs.next()) {
+            String userName = rs.getString(1);
+            String password = rs.getString(2);
+            String email = rs.getString(3);
+            int score = rs.getInt(4);
+            boolean status = rs.getBoolean(5);
+            boolean availableity = rs.getBoolean(6);
+            user = new UserTable(userName, password, email, score, status,availableity);
+        }
+            rs.close();
+            pst.close();
+            con.close();
+            return user;
+    }
+
+//    public static Vector<UserTable> isRepeated() throws SQLException {
+//        
+//        String query = "SELECT * FROM USERTABLE";
+//        PreparedStatement pst = con.prepareStatement(query);
+//        ResultSet rs = pst.executeQuery(query);
+//
+//        Vector<UserTable> userList = new Vector<>();
+//
+//        while (rs.next()) {
+//            String userName = rs.getString(1);
+//            String email = rs.getString(2);
+//            String password = rs.getString(3);
+//            int score = rs.getInt(4);
+//            boolean status = rs.getBoolean(5);
+//
+//            UserTable user = new UserTable(userName, password, email, score, status);
+//
+//            if (userList.stream().anyMatch(existingUser -> existingUser.getUserName().equals(userName))) {
+//                showAlert("Repeated User Name", "Duplicate user found", "Change");
+//            } else {
+//                userList.add(user);
+//            }
+//        }
+//        rs.close();
+//        pst.close();
+//        con.close();
+//        return userList;
+//    }
+    
+    
+    public static boolean isPlayerExist(String userName ,String password) throws SQLException     
+    {   
+        boolean flag =false;
+        
+        stablishConnection();
+
+        PreparedStatement pst = con.prepareStatement("SELECT USERNAME, PASSWORD FROM USERTABELM WHERE USERNAME = ? AND PASSWORD = ?");
+        
+        pst.setString(1, userName);
+        pst.setString(2, password);
+        
+        ResultSet rs = pst.executeQuery();
+        
+        while (rs.next()) 
+        {
+            String name = rs.getString(1);
+            String pass =  rs.getString(2);
+
+            if ((userName.equals(name)) && (password.equals(pass))) {
+                flag = true;
+                break;
+            } 
+        }
+        rs.close();
+        pst.close();
+        con.close();
+        
+       return flag; 
+    }
+    
+     public static void updateStatus(String userName,boolean status) throws SQLException {
+         
+        stablishConnection();
+        
+        String sql = "UPDATE USERTABELM SET  STATUS=? WHERE USERNAME=?";
+        
+        PreparedStatement pstm = con.prepareStatement(sql);  
+        
+        pstm.setBoolean(1, status);
+        pstm.setString(2,userName);
+        pstm.executeUpdate();
+        
+        pstm.close();
+        con.close();  
+    }
+     public static boolean isExist(String signUser)
+     {
+        try {
+
+                getAllData(); 
+                for (UserTable user:userList)
+                {
+                    userExist = user.userName.equals(signUser);
+                   if (userExist) {break;}
+                }
+            }catch (SQLException ex) {
+            Logger.getLogger(DAL_1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         return userExist;
+     } 
+            
+//    private static void showAlert(String body, String header, String buttonName) {
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle(body);
+//        alert.setHeaderText(header);
+//        ButtonType changeName = new ButtonType(buttonName);
+//        alert.getButtonTypes().setAll(changeName);
+//        alert.showAndWait();
+//    }
+        
+}
